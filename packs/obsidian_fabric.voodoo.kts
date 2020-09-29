834ac4d2dcde4bf1b86d0ef28b2d25c3cd1fb8cf
copygirl <copygirl@mcft.net>
2020-09-29 17:24:51 +0200

    Update modpack to 1.16.3
    Added mods:
    - Astromine 1.10.7
    - Chat Heads 0.1.2 (client-side)
    - Edit Sign 2.1.1
    - Heart of the Machine 0.1.6
    - Tech Reborn 3.5.4+build.122
    - The Bumblezone 2.2.5
    
    Removed mods:
    - Chains Link
    - Ducts
    - Towelette
    
    Updated mods:
    - Adorn 0.11.0 => 0.11.1
    - Bedspreads 1.1.0 => 1.2.0
    - Grind Enchantments 1.1.2 => 1.1.3
    - Immersive Portals 0.30 => 0.36
    - Linked Storage 1.3.7 => 1.3.9
    - Lithium 0.5.4 => 0.5.6
    - Modern Glass Doors 1.4.4 => 1.5.0
    - Netherite Horse Armor 0.4.2 => 0.4.3
    - Oh, How the Crafting Has Tabled 0.1.1 => 0.1.3
    - Oh The Biomes You'll Go 1.0.6 => 1.0.12
    - Repurposed Structures 1.5.4 => 1.6.1
    - Respawnable Pets ...
    - Roughly Enough Items 5.3.0 => 5.5.1
    - Roughly Enough Resources 2.1.1 => 2.1.2
    - ShulkerBoxTooltip 2.1.1 => 2.3.1
    - The Parakeet Mod 1.0.3 => 1.0.6
    - Better Enchanted Books 1.1.1 => 1.1.2
    - Illuminations 1.0.5 => 1.1
    - Item Scroller ...
    - Light Overlay 5.5.2 => 5.5.3
    - VoxelMap 1.10.10 => 1.10.11

--- packs/obsidian_fabric.voodoo.kts
+++ packs/obsidian_fabric.voodoo.kts
@@ -1,10 +1,10 @@
 title     = "obsidian 1.16 Fabric Modpack"
 authors   = listOf("copygirl")
-version   = "0.1.0"
+version   = "0.2.0"
 icon      = rootFolder.resolve("icon.png")
 
-mcVersion = "1.16.2"
-modloader { fabric(Fabric.intermediary.v_1_16_2) }
+mcVersion = "1.16.3"
+modloader { fabric(Fabric.intermediary.v_1_16_3) }
 
 pack {
     multimc {
@@ -17,11 +17,16 @@ root<Curse> {
 
     // COMMON MODS
     it.list {
-        common.validMcVersions   = setOf("1.16", "1.16.1")
+        // Due to 1.16.3 not being available in Twitch Launcher's internal API, we
+        // need to specify some file IDs manually to grab the newest version of mods.
+        common.validMcVersions   = setOf("1.16.1", "1.16.2", "1.16.3")
         common.invalidMcVersions = setOf("Forge")
 
+        // Libraries
+        +Mod.fabricApi { fileID = FileID(3066731) }
+
         // Optimization and Fixes
-        +Mod.lithium  // General-purpose server optimization
+        +Mod.lithium { fileID = FileID(3063230) } // General-purpose server optimization
         // TODO: +Mod.phosphor // Optimize the lighting engine
 
         // Tweaks and Utilities
@@ -30,13 +35,15 @@ root<Curse> {
         +Mod.hwyla                  // Shows what you're looking at
         +Mod.roughlyEnoughItems     // Like JEI, displays recipes and uses
         +Mod.roughlyEnoughResources // Like JER, displays block and mob drops
-        +Mod.shulkerboxtooltip      // Show shulker box contents when hovering over them
+        +Mod.shulkerboxtooltip      { fileID = FileID(3061113) } // Show shulker box contents when hovering over them
 
         // Gameplay Changes
         +Mod.anvilFix              // Removes the "Too Expensive" limit
-        +Mod.chainsLink            // Pistons move connected chain blocks
+        // TODO: Not compatible with 1.16.3
+        // +Mod.chainsLink            // Pistons move connected chain blocks
+        +Mod.editSign              { fileID = FileID(3053802) } // Allow editing signs in-place
         +Mod.fairenchanting        // Pay fairly in XP, rather than levels
-        +Mod.grindEnchantments     // Use Grindstone to remove enchantments
+        +Mod.grindEnchantments     { fileID = FileID(3063879) } // Use Grindstone to remove enchantments
         +Mod.stackablepotions      // Stack potions up to 16
         +Mod.suitablyStackableStew // Stews stack up to 16
 
@@ -45,47 +52,43 @@ root<Curse> {
         // ------------------
 
         // World Generation
-        +Mod.endRebellion               { common.validMcVersions = setOf("1.16.2") }
-                                        // Content for the End
-        // TODO: +Mod.heartOfTheMachine          // Dimension of abandoned machineries
+        +Mod.endRebellion               { common.invalidMcVersions = setOf("1.16.1") } // More content for the End
+        +Mod.heartOfTheMachine          // Dimension of abandoned machineries
         +Mod.ohTheBiomesYoullGoFabric   // Over 70 varied biomes
         +Mod.repurposedStructuresFabric // More structures and variants
-        // TODO: +Mod.theBumblezoneFabric        // Dimension full of beeeeeees!
+        +Mod.theBumblezoneFabric        // Dimension full of beeeeeees!
 
         // Decoration
-        +Mod.adorn                     // Decorate your home!
-        +Mod.bambooTweaks              // Bamboo building blocks
-        +Mod.bedspreadsFabric          // Banners on beds
+        +Mod.adorn                     { fileID = FileID(3063751) } // Decorate your home!
+        +Mod.bambooTweaksFabric        // Bamboo building blocks
+        +Mod.bedspreadsFabric          { fileID = FileID(3068187) } // Banners on beds
         +Mod.colorfulCut               // Slabs, stairs and walls made of concrete and terracotta
         +Mod.modernGlassDoors          // Adds glass door variants of Vanilla doors
         +Mod.ohHowTheCraftingHasTabled // Crafting Tables made of all the woods
         +Mod.woodsPlus                 // Additional wooden planks variants
 
         // Various
-        // TODO: +Mod.antiqueAtlas           // Map mod in an item
         +Mod.aquarius               // Expanding on Aquatic Update content
         +Mod.campanion              // Camping companion mod adding various things
         +Mod.doubleJumpMod          // Enchantment (level I to III) that adds double-jumps
-        +Mod.ducts                  // Expanding on Hoppers
-        +Mod.immersivePortalsMod    // See-through portals and seamless travel
-        +Mod.linkedStorage          // Basically Ender Storage
+        // TODO: Needs to be updated to newer Minecraft
+        // +Mod.ducts                  // Expanding on Hoppers
+        +Mod.immersivePortalsMod    { fileID = FileID(3063779) } // See-through portals and seamless travel
+        +Mod.linkedStorage          { fileID = FileID(3061753) } // Basically Ender Storage
         +Mod.netheriteHorseArmor    // Upgrade diamond horse armor to netherite tier
-        +Mod.respawnablePets        // Item that allows your pets to respawn when sleeping
+        +Mod.respawnablePets        { fileID = FileID(3057419) } // Item that allows your pets to respawn when sleeping
         +Mod.rswires                // RedPower2-like redstones wires and bundled cables
         +Mod.sandwichable           // Make custom sandwiches with many ingredients
         +Mod.theParakeetMod         // More birbs!
-        +Mod.towelette              // "Waterlog" most blocks with non-water fluids
+        // TODO: https://github.com/Virtuoel/Towelette/issues/33
+        // +Mod.towelette              // "Waterlog" most blocks with non-water fluids
         +Mod.wolvesWithArmor        // Adds armor for your (overly vulnerable) wolves
         +Mod.woodcutter             // Like the Stonecutter, but for wood!
 
         // Technology
-        // TODO: +Mod.astromineMain        // Space! (Not compatible with Immersive Portals)
-        // TODO: These mods (including Lacrimis) require Cotton Resources.
-        // +Mod.biomechanics         // Meat Machines
-        // +Mod.mechanizedSteamPower // Basically Flaxbeard's Steam Power
-
-        // Magic
-        // TODO: +Mod.lacrimis // Powered by Crying Obsidian
+        +Mod.astromineMain        // Space!
+        +Mod.reborncore           { fileID = FileID(3060564) } // Library mod for Tech Reborn
+        +Mod.techreborn           { fileID = FileID(3060565) } // IndustrialCraft inspired technology mod
 
         // SERVER OPTIONAL MODS
         group {
@@ -106,10 +109,11 @@ root<Curse> {
             group {
                 optional { selected = true }
             }.list {
-                +Mod.betterEnchantedBooks { description = "Unique looks for different enchantment books" }
+                +Mod.betterEnchantedBooks { common.invalidMcVersions = setOf("1.16.1"); description = "Unique looks for different enchantment books" }
                 +Mod.controllingForFabric { description = "Improves controls with search, showing conflicts" }
                 +Mod.dynamicFps           { description = "Reduce FPS when window is in background" }
-                +Mod.illuminations        { description = "Adds pretty-looking glowing parties" }
+                +Mod.illuminations        { fileID = FileID(3062304); description = "Adds pretty-looking glowing parties" }
+                // TODO: Immersive Portals not supported (yet?)
                 // +Mod.sodium               { description = "Render engine replacement (high FPS boost)" }
             }
 
@@ -117,11 +121,12 @@ root<Curse> {
             group {
                 optional { selected = false }
             }.list {
+                +Mod.chatHeads           { description = "See players' heads next to their chat messages" }
                 +Mod.dynamicSoundFilters { description = "Affects how sounds are played depending on location" }
                 +Mod.itemScroller        { description = "Use scroll wheel and other shortcuts to move items" }
                 +Mod.lightOverlay        { description = "Show blocks mobs can spawn by pressing F7" }
                 +Mod.presenceFootsteps   { description = "Footstep and shuffling sounds depending on materials" }
-                +Mod.voxelmap            { description = "Minimap and world map" }
+                +Mod.voxelmap            { fileID = FileID(3058801); description = "Minimap and world map" }
             }
         }
     }
