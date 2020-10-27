1cf4f95b8e1b5a070303753fe097498fb81e085b
copygirl <copygirl@mcft.net>
2020-10-27 13:49:01 +0100

    Update Tech Reborn 3.6.0 => 3.6.1

--- packs/obsidian_fabric.voodoo.kts
+++ packs/obsidian_fabric.voodoo.kts
@@ -1,6 +1,6 @@
 title     = "obsidian 1.16 Fabric Modpack"
 authors   = listOf("copygirl")
-version   = "0.3.0"
+version   = "0.3.1"
 icon      = rootFolder.resolve("icon.png")
 
 mcVersion = "1.16.3"
