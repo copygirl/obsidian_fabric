f2c60bd6f644b18dfa8bf608ab3eac57a50c45c9
copygirl <copygirl@mcft.net>
2020-10-31 10:18:39 +0100

    Update Fabrication config

--- packs/obsidian_fabric.voodoo.kts
+++ packs/obsidian_fabric.voodoo.kts
@@ -1,6 +1,6 @@
 title     = "obsidian 1.16 Fabric Modpack"
 authors   = listOf("copygirl")
-version   = "0.3.1"
+version   = "0.3.2"
 icon      = rootFolder.resolve("icon.png")
 
 mcVersion = "1.16.3"
